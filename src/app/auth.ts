49a1c60ef3c1bc8a07b235e6c204953bd8492364
skeie <trulsske@gmail.com>
2019-12-31 12:25:15 +0100

    Change to default error msg

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -2,6 +2,6 @@ import { ForbiddenError } from 'apollo-server';
 
 export const assertUser = user => {
   if (!user) {
-    throw new ForbiddenError('You must be logged in to track workout');
+    throw new ForbiddenError('Forbidden');
   }
 };
