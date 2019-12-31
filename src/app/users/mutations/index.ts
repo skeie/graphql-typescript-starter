b3d83c615b65a2d29ee395e3c22097f57c7b884a
skeie <trulsske@gmail.com>
2019-12-31 12:27:36 +0100

    Remove storeAPn stuff

--- src/app/users/mutations/index.ts
+++ src/app/users/mutations/index.ts
@@ -1,11 +1,9 @@
 import login from './loginUserMutation';
 import signup from './signupUserMutation';
 import updateUser from './updateUserMutation';
-import storeApnsToken from './storeApnsToken';
 
 export default {
   login,
   signup,
   updateUser,
-  storeApnsToken,
 };
